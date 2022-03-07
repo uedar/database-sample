0e22f02cbbecaf4b83ce47bebe130133345a413a
Ryo Ueda <u483198f@gmail.com>
2022-03-07 18:55:13 +0900

    update table

--- src/DetailDataCleaner.ts
+++ src/DetailDataCleaner.ts
@@ -3,16 +3,16 @@ type cellObject = {
     axis2: { x: number, y: number, z: number, periodic: boolean },
     axis3: { x: number, y: number, z: number, periodic: boolean }
 }
-type positionsObject = {
-    x: number, y: number, z: number
-}
-type forcesObject = {
-    x: number, y: number, z: number
+
+
+const sliceByNumber = (array: number[], number: number) => {
+    const length = Math.ceil(array.length / number)
+    return new Array(length).map((_, i) =>
+        array.slice(i * number, (i + 1) * number)
+    )
 }
 
 export default class DetailDataCleaner {
-
-
     public static getCell = (
         atomsJson: any
     ) => {
@@ -28,15 +28,14 @@ export default class DetailDataCleaner {
     public static getPostitions = (
         atomsJson: any
     ) => {
-        const cellArray: number[] = atomsJson["cell"]["array"]["__ndarray__"][2]
-        const pbcArray: boolean[] = atomsJson["pbc"]["__ndarray__"][2]
-        const cell: cellObject = {
-            axis1: { x: cellArray[0], y: cellArray[1], z: cellArray[2], periodic: pbcArray[0] },
-            axis2: { x: cellArray[3], y: cellArray[4], z: cellArray[5], periodic: pbcArray[1] },
-            axis3: { x: cellArray[6], y: cellArray[7], z: cellArray[7], periodic: pbcArray[2] }
-        }
-        return cell
+        const positionsArray: number[] = atomsJson["positions"]["__ndarray__"][2]
+        return sliceByNumber(positionsArray, 3)
     }
 
-
+    public static getForces = (
+        atomsJson: any
+    ) => {
+        const forcesArray: number[] = atomsJson["forces"]["__ndarray__"][2]
+        return sliceByNumber(forcesArray, 3)
+    }
 }
